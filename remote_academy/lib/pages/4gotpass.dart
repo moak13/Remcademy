256231903c8484116b285c3b32cbab0f417edefb
michael kalango <michaelkalango@gmail.com>
2019-09-02 23:08:39 +0100

    more application skeleton

---
+++ remote_academy/lib/pages/4gotpass.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ForgotPassPage extends StatefulWidget {
+  @override
+  _ForgotPassPageState createState() => _ForgotPassPageState();
+}
+
+class _ForgotPassPageState extends State<ForgotPassPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[],
+        ),
+      ),
+    );
+  }
+}
